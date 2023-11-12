ec48e787d53997768c01ffed92350ece9d5fe934
placebo-al <placebo_al@hotmail.com>
2023-11-12 17:43:08 +1100

    I have tried a few different things, mostly to do with css

---
+++ HTML-Version/Index-SAS.js
@@ -0,0 +1,29 @@
+
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+  connectedCallback() {
+this.innerHTML = `
+<script>
+const nav = document.querySelector('#main');
+let topOfNav = nav.offsetTop;
+
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.classList.remove('fixed-nav');
+    document.body.style.paddingTop = 0;
+  }
+}
+    
+window.addEventListener('scroll', fixNav);    
+</script>
+`;
+  }
+}
+
+customElements.define('header-component', Header);
